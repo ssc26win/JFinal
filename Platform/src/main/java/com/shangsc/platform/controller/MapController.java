1da607c7b7cba38ee997eab27fb25a53beeef30a
shangshichao@okay.cn <shangshichao@okay.cn>
2017-08-23 20:35:51 +0800

    Add map

---
+++ Platform/src/main/java/com/shangsc/platform/controller/MapController.java
@@ -0,0 +1,11 @@
+package com.shangsc.platform.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * @Author ssc
+ * @Date 2017/8/23 11:54
+ * @Desc 用途：
+ */
+public class MapController extends Controller {
+}
