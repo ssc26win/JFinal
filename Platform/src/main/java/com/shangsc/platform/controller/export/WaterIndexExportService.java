2023cf6ec6d4b0edd80957e6b5fbc62c5c0f146d
heheshang <2448318433@qq.com>
2017-08-23 09:13:12 +0800

    导出

---
+++ Platform/src/main/java/com/shangsc/platform/controller/export/WaterIndexExportService.java
@@ -0,0 +1,21 @@
+package com.shangsc.platform.controller.export;
+
+import com.jfinal.plugin.activerecord.Page;
+import com.shangsc.platform.model.WaterIndex;
+import com.shangsc.platform.model.WaterMeter;
+
+/**
+ * WaterIndex
+ *
+ * @author ssk www.hnapay.com Inc.All rights reserved
+ * @create 2017-08-22-下午 3:41
+ */
+public class WaterIndexExportService extends ExportBaseService{
+    private static final String FILE_NAME = "水表信息导出";
+
+
+    public String export(Page<WaterIndex> page) {
+
+        return null;
+    }
+}
