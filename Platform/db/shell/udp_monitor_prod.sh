9f2c1ff7ef2d425c6afc08c251b7bb91f74f4422
shangshichao <shangshichao>
2018-11-27 10:41:51 +0800

    Update 2.0

---
+++ Platform/db/shell/udp_monitor_prod.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# function:自动监控tomcat进程，挂了就执行重启操作
+# author:ssc
+# DEFINE
+ 
+# 获取udp PID
+UdpID=$(ps --no-heading -C java -f --width 1000 | grep "actual-udp.jar" | awk '{print $2}')
+ 
+# udp_startup
+StartUdp=/usr/local/tomcat7/actual-udp/bin/start.sh
+
+UdpMonitorLog=/usr/local/tomcat7/monitor/logs/Prod_UdpMonitor.log
+ 
+Monitor()
+{
+  echo "[info]开始监控udp...[$(date +'%F %H:%M:%S')]"
+  if [ "$UdpID" != "" ];then
+    echo "[info] udp 进程ID为:$UdpID."
+    echo "[info] udp 运行正常."
+  else
+    echo "[error]进程不存在!udp自动重启..."
+    echo "[info]$StartUdp,请稍候......"
+    $StartUdp
+  fi
+  echo "------------------------------"
+}
+Monitor>>$UdpMonitorLog
